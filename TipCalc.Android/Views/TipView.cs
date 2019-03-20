b4d1ce64c6ab815537778999e1e3e502053a4eee
Luís Fernando Moraes <luisfernando.pereira.moraes@gmail.com>
2019-03-19 22:56:36 -0300

    Add project files.

---
+++ TipCalc.Android/Views/TipView.cs
@@ -0,0 +1,17 @@
+﻿using Android.App;
+using Android.OS;
+using MvvmCross.Platforms.Android.Views;
+using TipCalc.Core.ViewModels;
+
+namespace TipCalc.Android.Views
+{
+    [Activity(Label = "Tip Calculator", MainLauncher = true,Theme = "@style/AppTheme")]
+    public class TipView : MvxActivity<TipViewModel>
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            SetContentView(Resource.Layout.tip_vew);
+        }
+    }
+}
